224f62dd4c3d41645edf514ba17767300448c5ec
SpqzM <nico.davy@gmail.com>
2017-11-02 16:21:26 +0100

    mail via formulaire -en cours-

--- inscription.php
+++ inscription.php
@@ -29,6 +29,30 @@ if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) &&
         'email' => $email));
 
     $manager->add($participant);
+    
+    $destinataire = 'safariz@yopmail.com';
+    // Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
+    $expediteur = $_POST['email'];
+     
+    $objet = "Inscription de" .$_POST['name'];
+     
+    $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+    $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
+    $headers .= 'To: '.$destinataire."\n"; // Mail de reponse
+    $headers .= 'From: "Nom_de_destinataire"<'.$expediteur.'>'."\n"; // Expediteur
+     
+    $message =  '<div style="width: 100%; text-align: center; font-weight: bold"> Bonjour '.$_POST['name'].'!<br>
+                    '.$_POST['message'].'</div>';
+     
+    if(mail($destinataire, $objet, $message, $headers))
+    {
+        echo '<script languag="javascript" >alert("Votre message a bien été envoyé ");</script>';
+    }
+    else // Non envoyé
+    {
+        echo '<script languag="javascript">alert("Votre message n\'a pas pu être envoyé");</script>';
+    }
+    header('Location: monformulaire.php');
 }
 ?>
 <div class="container">
