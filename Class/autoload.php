f793f1dd19016c3d6df26307f3dfabcb536699af
SpqzM <nico.davy@gmail.com>
2017-11-07 14:43:10 +0100

    autoload modifié

--- Class/autoload.php
+++ Class/autoload.php
@@ -6,5 +6,12 @@ function autoload($classname) {
     if (file_exists($file = __DIR__ . '/' . $classname . '.php')) {
         require $file;
     }
+    
+        elseif (file_exists($file = __DIR__ . '/' . strtolower($classname) . '.php')) {
+            require $file;
+    }
+    
+    
 }
+
 spl_autoload_register('autoload');
