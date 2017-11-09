e74814dab07e497f239770f60f349427631a979c
Sylvain <sylvainprat75@gmail.com>
2017-11-09 13:13:42 +0100

    push du 9/11

--- views/footer.php
+++ views/footer.php
@@ -1,13 +1,13 @@
 <footer class="container-fluid">
     <div class="row">
 
-        <div class='col-sm-8'>
+        <div class='col-sm-8 col-md-6'>
             <img class="img-responsive" src="img/igp.png" alt="igp.png" />
             <img class="img-responsive" src="img/rdc.png" alt="rdc.png" />
             <div id="compte_a_rebours"><noscript>Fin de l'évènement.</noscript></div>
         </div>
         
-        <div class='col-sm-4'>
+        <div class='col-sm-4 col-md-6 '>
             <nav class="navbar navbar-expand" role="navigation">
                 <ul class="nav navbar-nav">
                     <li><a href="index.php" class="btn btn-form" role="button">Accueil</a></li>
